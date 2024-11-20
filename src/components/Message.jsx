dfb8ee75efe8b848bb49d8d541de757dc2d89896
Sinclair <sinclairolajuwon@gmail.com>
2024-11-20 12:00:51 -0600

    starting

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,14 @@
 const Message = () => {
+    const mess = [
+        "Learn React",
+        "Apply for jobs",
+        "Invest your new income"
+    ]
+    const step = 1
   return (
+  
     <div className="mt-8 p-6 bg-white rounded-lg shadow-lg text-center text-gray-800">
-      <p className="text-lg font-medium">This is your message!</p>
+      <p className="text-lg font-medium">{}</p>
     </div>
   );
 };
